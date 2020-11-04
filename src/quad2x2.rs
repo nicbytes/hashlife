71455625107d2144f1ede22eb87580850beb5f3c
Nicholas Buckeridge <bucknich@gmail.com>
2020-11-04 13:34:46 +1000

    Added quad2x2

--- src/quad2x2.rs
+++ src/quad2x2.rs
@@ -4,7 +4,12 @@ use crate::quad::Quadrant;
 
 use std::rc::Rc;
 use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
 use std::hash::{Hash, Hasher};
+
+type ReferenceCounter<T> = Rc<T>;
+type RcQuad2x2 = ReferenceCounter<Quad2x2>;
+type RcQuad4x4 = ReferenceCounter<Quad4x4>;
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub enum CellOf2x2 {
@@ -14,35 +19,40 @@ pub enum CellOf2x2 {
 }
 
 impl CellOf2x2 {
-    fn is_none(&self) -> bool {
+    pub fn is_none(&self) -> bool {
         match self {
             CellOf2x2::None => true,
             _ => false,
         }
     }
 
-    fn is_automata(&self) -> bool {
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.is_none()
+    }
+
+    pub fn is_automata(&self) -> bool {
         match self {
             CellOf2x2::Automata(_) => true,
             _ => false,
         }
     }
 
-    fn is_dead(&self) -> bool {
+    pub fn is_dead(&self) -> bool {
         match self {
             CellOf2x2::Automata(a) => a.is_dead(),
             _ => false,
         }
     }
 
-    fn is_alive(&self) -> bool {
+    pub fn is_alive(&self) -> bool {
         match self {
             CellOf2x2::Automata(a) => a.is_alive(),
             _ => false,
         }
     }
 
-    fn is_reference(&self) -> bool {
+    pub fn is_reference(&self) -> bool {
         match self {
             CellOf2x2::Reference(_) => true,
             _ => false,
@@ -57,11 +67,11 @@ pub enum Direction {
 
 struct Quad2x2 {
     quadrant: Quadrant,
-    parent: Option<Rc<Quad4x4>>,
-    nw: Rc<CellOf2x2>,
-    ne: Rc<CellOf2x2>,
-    sw: Rc<CellOf2x2>,
-    se: Rc<CellOf2x2>,
+    parent: Option<RcQuad4x4>,
+    nw: CellOf2x2,
+    ne: CellOf2x2,
+    sw: CellOf2x2,
+    se: CellOf2x2,
     hash: u64,
 }
 
@@ -71,52 +81,155 @@ impl Hash for Quad2x2 {
     }
 }
 
-fn calculate_hash(quadrant: &Quadrant, nw: &Rc<CellOf2x2>, ne: &Rc<CellOf2x2>, sw: &Rc<CellOf2x2>, se: &Rc<CellOf2x2>) -> u64 {
+fn calculate_hash(quadrant: &Quadrant, nw: &CellOf2x2, ne: &CellOf2x2, sw: &CellOf2x2, se: &CellOf2x2) -> u64 {
     let mut state = DefaultHasher::new();
     quadrant.hash(&mut state);
     nw.hash(&mut state);
     ne.hash(&mut state);
     sw.hash(&mut state);
     se.hash(&mut state);
-    state.finish();
+    state.finish()
 }
 
-use cached::SizedCache;
+/// Whenever creating or crafting a Quad2x2 in any way, use the factory as a
+/// single point of reference to ensure the result is made as fast as
+/// possible.
+struct Factory2x2 {
+    cache: HashMap<u64, RcQuad2x2>,
+}
 
+impl Factory2x2 {
+    /// Create a new Quad 2x2 cache factory.
+    fn new() -> Self {
+        Self { cache: HashMap::new() }
+    }
 
-impl Quad2x2 {
-    cached_key! {
-        JOIN: SizedCache<u64, Rc<Self>> = SizedCache::with_size(1024);
-        Key = { calculate_hash(&quadrant, &nw, &ne, &sw, &se) };
+    pub fn join(&mut self, parent: Option<RcQuad4x4>, quadrant: Quadrant, nw: CellOf2x2, ne: CellOf2x2, sw: CellOf2x2, se: CellOf2x2) -> RcQuad2x2 {
+        // pre-compute hash
+        let hash = calculate_hash(&quadrant, &nw, &ne, &sw, &se);
 
-        fn join(parent: Option<Rc<Quad4x4>>, quadrant: Quadrant, nw: Rc<CellOf2x2>, ne: Rc<CellOf2x2>, sw: Rc<CellOf2x2>, se: Rc<CellOf2x2>) -> Rc<Self> {
-            // pre-compute hash
-            let hash = calculate_hash(&quadrant, &nw, &ne, &sw, &se);
+        // Check if already exists.
+        if let Some(ref_to_quad) = self.cache.get(&hash) {
+            return ReferenceCounter::clone(ref_to_quad);
+        }
 
-            Rc::new(Self {
-                quadrant,
-                parent,
-                nw, ne, sw, se,
-                hash,
-            })
+        let quad = ReferenceCounter::new(Quad2x2 {
+            quadrant,
+            parent,
+            nw, ne, sw, se,
+            hash,
+        });
+
+        // Save the newly created quad in the cache.
+        self.cache.insert(hash, ReferenceCounter::clone(&quad));
+        quad
+    }
+
+    /// Create a quad with empty children.
+    pub fn empty(&mut self, parent: Option<RcQuad4x4>, quadrant: Quadrant) -> RcQuad2x2 {
+        // make empty automatas
+        let e = CellOf2x2::None;
+        // pre-compute hash
+        let hash = calculate_hash(&quadrant, &e, &e, &e, &e);
+
+        // Check if already exists.
+        if let Some(ref_to_quad) = self.cache.get(&hash) {
+            return ReferenceCounter::clone(ref_to_quad);
+        }
+
+        let quad = ReferenceCounter::new(Quad2x2 {
+            quadrant,
+            parent,
+            nw: e, ne: e, sw: e, se: e,
+            hash,
+        });
+
+        // Save the newly created quad in the cache.
+        self.cache.insert(hash, ReferenceCounter::clone(&quad));
+        quad
+    }
+
+    /// Create a quad with dead automata.
+    pub fn dead(&mut self, parent: Option<RcQuad4x4>, quadrant: Quadrant) -> RcQuad2x2 {
+        // make dead automatas
+        let d = CellOf2x2::Automata(Automata::Dead);
+        // pre-compute hash
+        let hash = calculate_hash(&quadrant, &d, &d, &d, &d);
+
+        // Check if already exists.
+        if let Some(ref_to_quad) = self.cache.get(&hash) {
+            return ReferenceCounter::clone(ref_to_quad);
+        }
+
+        let quad = ReferenceCounter::new(Quad2x2 {
+            quadrant,
+            parent,
+            nw: d, ne: d, sw: d, se: d,
+            hash,
+        });
+
+        // Save the newly created quad in the cache.
+        self.cache.insert(hash, ReferenceCounter::clone(&quad));
+        quad
+    }
+
+    fn random_alive_dead_cell(&self) -> CellOf2x2 {
+        let mut automata = [0u8; 1];
+        getrandom::getrandom(&mut automata).expect("failed to gen rand automata");
+        if automata[0] % 2 == 0 {
+            CellOf2x2::Automata(Automata::Dead)
+        } else {
+            CellOf2x2::Automata(Automata::Alive)
         }
     }
+
+    pub fn rand(&mut self, parent: Option<RcQuad4x4>, quadrant: Quadrant) -> RcQuad2x2 {
+        // collect random automata
+        let nw = self.random_alive_dead_cell();
+        let ne = self.random_alive_dead_cell();
+        let sw = self.random_alive_dead_cell();
+        let se = self.random_alive_dead_cell();
+
+        // pre-compute hash
+        let hash = calculate_hash(&quadrant, &nw, &ne, &sw, &se);
+
+        // Check if already exists.
+        if let Some(ref_to_quad) = self.cache.get(&hash) {
+            return ReferenceCounter::clone(ref_to_quad);
+        }
+
+        let quad = ReferenceCounter::new(Quad2x2 {
+            quadrant,
+            parent,
+            nw, ne, sw, se,
+            hash,
+        });
+
+        // Save the newly created quad in the cache.
+        self.cache.insert(hash, ReferenceCounter::clone(&quad));
+        quad
+    }
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn make_factory() -> Factory2x2 {
+        Factory2x2 { cache: HashMap::new() }
+    }
+
     #[test]
     fn make_empty_top_2x2() {
-        let e = Quad2x2::empty(None, Quadrant::TOP);
+        let e = make_factory().empty(None, Quadrant::TOP);
         assert_eq!(e.quadrant, Quadrant::TOP);
         assert!(e.parent.is_none());
     }
 
     #[test]
     fn make_empty_2x2_with_empty_children() {
-        let e = Quad2x2::empty(None, Quadrant::TOP);
+        let e = make_factory().empty(None, Quadrant::TOP);
         assert!(e.nw.is_empty());
         assert!(e.ne.is_empty());
         assert!(e.sw.is_empty());
@@ -125,14 +238,14 @@ mod tests {
 
     #[test]
     fn make_dead_top() {
-        let x = Quad2x2::dead(None, Quadrant::TOP);
+        let x = make_factory().dead(None, Quadrant::TOP);
         assert_eq!(x.quadrant, Quadrant::TOP);
         assert!(x.parent.is_none());
     }
 
     #[test]
     fn make_dead_2x2_with_empty_children() {
-        let x = Quad2x2::dead(None, Quadrant::TOP);
+        let x = make_factory().dead(None, Quadrant::TOP);
         assert!(x.nw.is_dead());
         assert!(x.ne.is_dead());
         assert!(x.sw.is_dead());
@@ -141,14 +254,14 @@ mod tests {
 
     #[test]
     fn make_rand_top() {
-        let x = Quad2x2::rand(None, Quadrant::TOP);
+        let x = make_factory().rand(None, Quadrant::TOP);
         assert_eq!(x.quadrant, Quadrant::TOP);
         assert!(x.parent.is_none());
     }
 
     #[test]
     fn make_rand_2x2_with_empty_children() {
-        let x = Quad2x2::rand(None, Quadrant::TOP);
+        let x = make_factory().rand(None, Quadrant::TOP);
         assert!(x.nw.is_dead() || x.nw.is_alive());
         assert!(x.ne.is_dead() || x.ne.is_alive());
         assert!(x.sw.is_dead() || x.sw.is_alive());
@@ -156,15 +269,15 @@ mod tests {
     }
 
     #[test]
-    fn make_2x2_from() {
+    fn make_2x2_join() {
         let x1 = CellOf2x2::Automata(Automata::Dead);
         let x2 = CellOf2x2::Reference(Direction::E);
         let x3 = CellOf2x2::Automata(Automata::Alive);
         let x4 = CellOf2x2::Reference(Direction::E);
-        let x = Quad2x2::join(None, Quadrant::TOP, x1, x2, x3, x4);
+        let x = make_factory().join(None, Quadrant::TOP, x1, x2, x3, x4);
         assert!(x.nw.is_automata());
         assert!(x.nw.is_dead());
-        assert!(x.sw.is_dead());
+        assert!(x.sw.is_alive());
         assert!(x.ne.is_reference());
         assert!(x.se.is_reference());
 
